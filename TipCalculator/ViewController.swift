02bd1e86d48848171e6a04d511671a9bfd3a414d
Juliang Li <juliang0705@gmail.com>
2015-12-02 15:41:36 -0600

    Improve UI and add callback functions

--- TipCalculator/ViewController.swift
+++ TipCalculator/ViewController.swift
@@ -7,18 +7,57 @@
 //
 
 import UIKit
+import Foundation
+class ViewController: UIViewController,UITextFieldDelegate {
 
-class ViewController: UIViewController {
-
+    @IBOutlet var amountTextField: UITextField!
+    @IBOutlet var tipSlider: UISlider!
+    @IBOutlet var tipPercentageLabel: UILabel!
+    @IBOutlet var tipAmountLabel: UILabel!
+    @IBOutlet var totalLabel: UILabel!
+    @IBOutlet var tipSegments: UISegmentedControl!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        amountTextField.delegate = self
+        amountTextField.keyboardType = UIKeyboardType.NumberPad
+        amountTextField.textAlignment = NSTextAlignment.Right
+        amountTextField.becomeFirstResponder()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    @IBAction func amountEditing(sender: UITextField) {
+        var number:String = sender.text
+        var length:Int = (number as NSString).length
+        switch length{
+            case 0:
+                number = "0.00"
+            case 1:
+                number = "0.0" + number
+          //  case 2:
+          //      number = "0." + number
+            default:
+                number.removeAtIndex(advance(number.endIndex,-2))
+                number.insert(".", atIndex: advance(number.endIndex,-2))
+        }
+        sender.text = number
+    }
+    @IBAction func tipSliderValueChanged(sender: UISlider) {
+        let value:Int = Int(sender.value)
+        tipPercentageLabel.text = String(value) + "%";
+    }
+    @IBAction func tipSegmentsValueChanged(sender: UISegmentedControl) {
+        let index:Int = sender.selectedSegmentIndex
+        // minimal tip is 10% and increment by 5%
+        let value:Int = 10 + 5 * index
+        tipSlider.setValue(Float(value), animated: true)
+        tipSlider.sendAction("tipSliderValueChanged:", to: self, forEvent: nil)
+    }
+    
 
 
 }
