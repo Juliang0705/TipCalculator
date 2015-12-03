48d3a97c0bfa45b3f03c5e561b7a9543726f2760
Juliang Li <juliang0705@gmail.com>
2015-12-02 21:43:17 -0600

    Add setting view

--- TipCalculator/ViewController.swift
+++ TipCalculator/ViewController.swift
@@ -16,32 +16,25 @@ class ViewController: UIViewController,UITextFieldDelegate {
     @IBOutlet var tipAmountLabel: UILabel!
     @IBOutlet var totalLabel: UILabel!
     @IBOutlet var tipSegments: UISegmentedControl!
-    
+    var settings:SettingView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         amountTextField.delegate = self
         amountTextField.keyboardType = UIKeyboardType.DecimalPad
         amountTextField.textAlignment = NSTextAlignment.Right
         amountTextField.becomeFirstResponder()
+        settings = NSBundle.mainBundle().loadNibNamed("SettingView", owner: self, options: nil).last as! SettingView
+        settings.frame = CGRectMake(0, -self.view.frame.size.height + 40, self.view.frame.size.width, self.view.frame.size.height)
+        self.view.addSubview(settings)
+        settings.setup()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    @IBAction func amountEditing(sender: UITextField) {
-        // make sure the dot only appears at most once
-        var dotCount:Int = 0
-        for c in Array(amountTextField.text) {
-            println(c)
-            if (c == "."){
-                dotCount += 1
-            }
-        }
-        if (dotCount > 1){
-            amountTextField.text.removeAtIndex(amountTextField.text.endIndex.predecessor())
-            return
-        }
+    func calculateTips(){
         // do the calculation
         let amount:Double = (amountTextField.text as NSString).doubleValue
         let percentage:Double = (tipPercentageLabel.text! as NSString).doubleValue
@@ -50,12 +43,28 @@ class ViewController: UIViewController,UITextFieldDelegate {
         let total:Double = (amountTextField.text as NSString).doubleValue + tip
         totalLabel.text = String(format:"%.2f",total)
     }
+    @IBAction func amountEditing(sender: UITextField) {
+        // make sure the dot only appears at most once
+        var dotCount:Int = 0
+        for c in Array(amountTextField.text) {
+            if (c == "."){
+                dotCount += 1
+            }
+        }
+        if (dotCount > 1){
+            amountTextField.text.removeAtIndex(amountTextField.text.endIndex.predecessor())
+            return
+        }
+        self.calculateTips()
+    }
     
     @IBAction func tipSliderValueChanged(sender: UISlider) {
         let value:Int = Int(tipSlider.value)
         tipPercentageLabel.text = String(value) + "%";
         // unselect the tip segment to avoid confusion
         tipSegments.selectedSegmentIndex = -1
+        self.calculateTips()
+        
     }
     @IBAction func tipSegmentsValueChanged(sender: UISegmentedControl) {
         let index:Int = tipSegments.selectedSegmentIndex
